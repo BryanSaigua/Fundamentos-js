1cdb146306da98f513b4e01d9718f86a91fcc916
BryanSaigua <bryan-alexander2013@hotmail.com>
2020-07-01 23:46:42 +0200

    variables de js

---
+++ clase1.js
@@ -0,0 +1,10 @@
+//console.log('Hola mundo Js')
+
+var nombre = 'Bryan'  //declarando varibales
+var apellido = "Saigua"
+var edad = 28
+edad = '28 anos'
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad )
+
+var peso = 75
